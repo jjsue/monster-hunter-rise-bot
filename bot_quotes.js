d8094bd5f709e5638abb7a8c517d7408b22ea1ed
Jjsue <jjsue_95@hotmail.es>
2020-09-21 13:29:05 +0200

    Algunas quotes añadidas

--- bot_quotes.js
+++ bot_quotes.js
@@ -2,5 +2,9 @@ const quotesList = [
     "Tu compañero siempre se lleva todas las joyas que a ti te hacen falta.",
     "Se me han acabado las megapociones.",
     "Se me han olvidado las piedras de afilar.",
+    "Oh si, machalita.",
+    "Si joder le he cortado la cola. *Looteas* Escama.",
+    "'Nadie mejor que tú para el trabajo'. El cansino de la caravana del 4u",
+    "\"WHERE'S MY DRAGONATOR?\". Jefe de la Comisión"
 ]
 module.exports = quotesList;
